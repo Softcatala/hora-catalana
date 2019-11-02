36c6423d4ae1fccaf684fb70128b72af4fa6183e
Jaume Ortolà <jaumeortola@gmail.com>
2019-11-02 09:59:35 +0100

    mig quart (ben) passat

--- hora.js
+++ hora.js
@@ -10,29 +10,29 @@ function get_time_campanar_tradicional(hour, minute) {
     if (minute == 2 || minute == 3 || minute == 4) return get_article(hour) + " " + get_hour(hour) + " " + get_tocades(hour);
     if (minute == 5 || minute == 6) return get_article(hour) + " " + get_hour(hour) + " ben " + get_tocades(hour);
     if (minute == 7 || minute == 8) return "Mig quart " + get_de(hour) + get_hour(hour);
-    if (minute == 9 || minute == 10 || minute == 11) return "Mig quart tocat " + get_de(hour) + get_hour(hour);
-    if (minute == 12 || minute == 13) return "Mig quart ben tocat " + get_de(hour) + get_hour(hour);
+    if (minute == 9 || minute == 10 || minute == 11) return "Mig quart passat " + get_de(hour) + get_hour(hour);
+    if (minute == 12 || minute == 13) return "Mig quart ben passat " + get_de(hour) + get_hour(hour);
     if (minute == 14 || minute == 15 || minute == 16) return "Un quart " + get_de(hour) + get_hour(hour);
 
     if (minute == 17 || minute == 18 || minute == 19) return "Un quart tocat " + get_de(hour) + get_hour(hour);
     if (minute == 20 || minute == 21) return "Un quart ben tocat " + get_de(hour) + get_hour(hour);
     if (minute == 22 || minute == 23) return "Un quart i mig " + get_de(hour) + get_hour(hour);
-    if (minute == 24 || minute == 25 || minute == 26) return "Un quart i mig tocat " + get_de(hour) + get_hour(hour);
-    if (minute == 27 || minute == 28) return "Un quart i mig ben tocat " + get_de(hour) + get_hour(hour);
+    if (minute == 24 || minute == 25 || minute == 26) return "Un quart i mig passat " + get_de(hour) + get_hour(hour);
+    if (minute == 27 || minute == 28) return "Un quart i mig ben passat " + get_de(hour) + get_hour(hour);
     if (minute == 29 || minute == 30 || minute == 31) return "Dos quarts " + get_de(hour) + get_hour(hour);
 
     if (minute == 32 || minute == 33 || minute == 34) return "Dos quarts tocats " + get_de(hour) + get_hour(hour);
     if (minute == 35 || minute == 36) return "Dos quarts ben tocats " + get_de(hour) + get_hour(hour);
     if (minute == 37 || minute == 38) return "Dos quarts i mig " + get_de(hour) + get_hour(hour);
-    if (minute == 39 || minute == 40 || minute == 41) return "Dos quarts i mig tocats " + get_de(hour) + get_hour(hour);
-    if (minute == 42 || minute == 43) return "Dos quarts i mig ben tocats " + get_de(hour) + get_hour(hour);
+    if (minute == 39 || minute == 40 || minute == 41) return "Dos quarts i mig passats " + get_de(hour) + get_hour(hour);
+    if (minute == 42 || minute == 43) return "Dos quarts i mig ben passats " + get_de(hour) + get_hour(hour);
     if (minute == 44 || minute == 45 || minute == 46) return "Tres quarts " + get_de(hour) + get_hour(hour);
 
     if (minute == 47 || minute == 48 || minute == 49) return "Tres quarts tocats " + get_de(hour) + get_hour(hour);
     if (minute == 50 || minute == 51) return "Tres quarts ben tocats " + get_de(hour) + get_hour(hour);
     if (minute == 52 || minute == 53) return "Tres quarts i mig " + get_de(hour) + get_hour(hour);
-    if (minute == 54 || minute == 55 || minute == 56) return "Tres quarts i mig tocats " + get_de(hour) + get_hour(hour);
-    if (minute == 57 || minute == 58) return "Tres quarts i mig ben tocats " + get_de(hour) + get_hour(hour);
+    if (minute == 54 || minute == 55 || minute == 56) return "Tres quarts i mig passats " + get_de(hour) + get_hour(hour);
+    if (minute == 57 || minute == 58) return "Tres quarts i mig ben passats " + get_de(hour) + get_hour(hour);
     if (minute == 59) return get_article(hour) + " " + get_hour(hour)
 
     return "no ho sé";
